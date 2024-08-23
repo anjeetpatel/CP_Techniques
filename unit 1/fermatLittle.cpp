3944ea0ec0205a0d11753b681c3d5fdd47ddbe78
anjeetpatel <anjeetpatel0306@gmail.com>
2024-08-23 15:45:49 +0530

    unit1

--- unit 1/fermatLittle.cpp
+++ unit 1/fermatLittle.cpp
@@ -1,24 +1,29 @@
-//Prove that fermat little theorem does not hold for p = 6 and a = 2 ?
 #include <iostream>
+#include <cmath>
 using namespace std;
-
-int powerMod(int a, int n, int p)
+bool isFermatLittle(int n, int a)
 {
-    int res = 1;
-    for (int i = 0; i < n; i++)
-        res = (res * a) % p;
-    return res;
+    if ((int)pow(a, n - 1) % n == 1)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
-
 int main()
 {
-    int a = 2;
-    int p = 6;
-    int result = powerMod(a, p - 1, p);
-    cout << "2^(" << p - 1 << ") mod " << p << " = " << result << endl;
-    if (result == 1)
-        cout << "Fermat's Little Theorem holds for p = " << p << " and a = " << a << endl;
+    int n;
+    int a;
+    cin >> n >> a;
+    if (isFermatLittle(a, n))
+    {
+        cout << "True";
+    }
     else
-        cout << "Fermat's Little Theorem does not hold for p = " << p << " and a = " << a << endl;
+    {
+        cout << "False";
+    }
     return 0;
 }
