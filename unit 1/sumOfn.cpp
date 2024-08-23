d7773dc357ed4f12b0e7b9ceeda3be54c74eb049
anjeetpatel <anjeetpatel0306@gmail.com>
2024-08-23 15:29:31 +0530

    unit1

---
+++ unit 1/sumOfn.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    // int sum = 0;
+    // for (int i = 0; i <= n; i++)
+    // {
+    //     sum += i;
+    // }
+    int sum1 = (n * (n + 1)) / 2;
+    cout << sum1;
+}
